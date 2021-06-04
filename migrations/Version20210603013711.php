886b6816715021306f9053f608ed256e4fb77a66
Rodney Camp <rcamp7979@gmail.com>
2021-06-04 00:15:52 -0400

    Updating files

---
+++ migrations/Version20210603013711.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210603013711 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE card (id INT AUTO_INCREMENT NOT NULL, manufacturer VARCHAR(255) DEFAULT NULL, year INT DEFAULT NULL, setname VARCHAR(255) DEFAULT NULL, player VARCHAR(255) DEFAULT NULL, cardnum INT DEFAULT NULL, variation VARCHAR(255) DEFAULT NULL, value VARCHAR(255) DEFAULT NULL, user VARCHAR(255) NOT NULL, orderdate DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE card');
+        $this->addSql('ALTER TABLE `user` CHANGE roles roles LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_bin`');
+    }
+}
