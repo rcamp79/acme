54d9c0a45445588ae68d1a554a0dcea61a0bd71b
Rodney Camp <rcamp7979@gmail.com>
2021-06-03 23:43:18 -0400

    Order detail page done. Ready to deploy.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -17,7 +17,7 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('home.html.twig');
+        return $this->render('card');
     }
 
 }
--- src/Controller/OrdersController.php
+++ src/Controller/OrdersController.php
@@ -4,6 +4,8 @@
 namespace App\Controller;
 
 
+use App\Entity\Card;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,12 +17,22 @@ class OrdersController extends AbstractController
     /**
      * @Route("/order", name="order")
      */
-    public function order(Request $request): Response
+    public function order(Request $request, EntityManagerInterface $em): Response
     {
+        $user = $this->getUser()->getUserIdentifier();
+        $repository = $em->getRepository(Card::class);
 
+        $data = $repository->findBy(['user' => $user]);
+
+        if (isset($data)) {
+            return $this->render('order.html.twig', [
+                'data' => $data
+            ]);
+        }
 
         return $this->render('order.html.twig');
     }
+
 
 
 }
