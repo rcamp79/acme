8cfc64b466f340ddc7df116964a57c9e3db6d9c9
Rodney Camp <rcamp7979@gmail.com>
2021-06-04 20:23:18 -0400

    fixing deployment issues

--- public/index.php
+++ public/index.php
@@ -4,6 +4,12 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+
+
 return function (array $context) {
+
+    $_SERVER[‘APP_ENV’]=’prod’;
+    $_SERVER['DATABASE_URL']='mysql://sql5416954:uUIDQuvlSl@sql5.freemysqlhosting.net:3306/sql5416954?serverVersion=5.5.62';
+
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
