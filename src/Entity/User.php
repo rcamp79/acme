36df96b616c0a54bc1185d496e8074e7675f08d0
Rodney Camp <rcamp7979@gmail.com>
2021-06-05 13:00:45 -0400

    heroku tweaking

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -125,4 +125,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
     }
+
+    public function getUsername()
+    {
+        // TODO: Implement getUsername() method.
+    }
 }
